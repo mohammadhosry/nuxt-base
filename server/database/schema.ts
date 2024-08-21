f1dd9cf147f342b9c19713575fca1059014fb89f
mohammadhosry <mohammad.hosry@gmail.com>
2024-08-21 20:03:17 +0300

    products schema  fix + migrate test

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -3,6 +3,6 @@ import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core'
 export const products = sqliteTable('products', {
     id: integer('id').primaryKey({ autoIncrement: true }),
     name: text('name').notNull(),
-    avatar: text('image'),
+    image: text('image'),
     createdAt: integer('created_at', { mode: 'timestamp' }).notNull(),
 })
