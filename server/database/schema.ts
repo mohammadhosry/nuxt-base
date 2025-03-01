f274ca2df63eb6ec5c7320d9df1d168bfd3f92b4
mohammadhosry <mohammad.hosry@gmail.com>
2025-03-02 00:53:59 +0200

    users table

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,5 +1,11 @@
 import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core'
 
+export const users = sqliteTable('users', {
+    id: integer('id').primaryKey({ autoIncrement: true }),
+    email: text('email').notNull().unique(),
+    password: text('password').notNull(),
+})
+
 export const products = sqliteTable('products', {
     id: integer('id').primaryKey({ autoIncrement: true }),
     name: text('name').notNull(),
